d3c47d4bad203de5b8f90f2672c26ff94e9a40f3
pavankumar <pavan.kumar@vapprtech.com>
2022-01-12 17:02:58 +0530

    updated component scan

--- src/main/java/com/arrivnow/customermanagement/customermanagement/CustomermanagementApplication.java
+++ src/main/java/com/arrivnow/customermanagement/customermanagement/CustomermanagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@ComponentScan(basePackages={"com.customermanagement"})
 public class CustomermanagementApplication
 {
 
