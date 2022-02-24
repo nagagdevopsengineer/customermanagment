7a6404017cf115883cbe5c45bfbafa64927cf5a4
pawan <p@vapprtech.com>
2022-02-25 00:19:25 +0530

    pom.xml

--- src/main/java/com/arrivnow/customermanagement/customermanagement/CustomermanagementApplication.java
+++ src/main/java/com/arrivnow/customermanagement/customermanagement/CustomermanagementApplication.java
@@ -2,6 +2,7 @@ package com.arrivnow.customermanagement.customermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomermanagementApplication
 {
+    @GetMapping("/test")
+    public String test()
+    {
+
+        return "test";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CustomermanagementApplication.class, args);
